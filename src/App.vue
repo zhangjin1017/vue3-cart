7400ca57e1f2685a1b1893d3133ce2e698454a0c
zj <1216916137@qq.com>
2022-06-23 00:38:24 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>App 根组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyApp'
+}
+</script>
+
+<style lang="less" scoped>
+:root{
+  font-size:12px;
+}
+</style>
