aaccfe98694b0f1a4bd38ba8d94f0c9072dd28f7
zj <1216916137@qq.com>
2022-06-23 11:30:26 +0800

    完成

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/css/bootstrap.css'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+
+const app=createApp(App)
+
+axios.defaults.baseURL='https://www.escook.cn'
+app.config.globalProperties.$http=axios
+
+app.mount('#app')
